b297f6bb65807e19aedbf0ed29e16058b337dff1
phzz-starboy <contato.pedroflawz@gmail.com>
2024-01-31 11:55:03 -0300

    projeto que deu errado K
    Um dia eu arrumo isso, mas vou deixar publico no repositório pelo tempo que eu levei pra fazer

---
+++ js/orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        productName: 'The greatest product of all time',
+        productNumber: '85743',
+        paymentStatus: 'Due',
+        status: 'Pending'
+    },
+    {
+        productName: 'The biggest Selection of products',
+        productNumber: '97245',
+        paymentStatus: 'Refunded',
+        status: 'Declined'
+    },
+    {
+        productName: 'Marry poppins lmao sample',
+        productNumber: '36452',
+        paymentStatus: 'Paid',
+        status: 'Active'
+    },
+]
